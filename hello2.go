aeb10564e3171e8fb44b227cc499171978bf283f
Steve Church <sacfxdx@bellsouth.net>
2016-05-11 13:31:55 -0400

    test update

--- hello2.go
+++ hello2.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func main() {
     fmt.Printf("xx - hello2, world\n")
+    fmt.Printf("xx - hello2, world\n")
+    fmt.Printf("xx - hello2, world\n")
+    fmt.Printf("xx - hello2, world\n")
+    fmt.Printf("xx - hello2, world\n")
+    fmt.Printf("xx - hello2, world\n")
 }
